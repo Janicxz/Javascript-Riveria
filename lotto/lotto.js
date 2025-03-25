0fc04de11618c3d03bd7c94bf40f5a681e70c6aa
Janicxz <janizu99@gmail.com>
2025-03-25 05:11:09 +0200

    More work on lotto program

--- lotto/lotto.js
+++ lotto/lotto.js
@@ -1,16 +1,17 @@
-const RIVIT_MAARA = 7;
-const RIVIT_YLIN = 39;
-
 // Arpoo lottorivin ja luo sille taulukon
-const ArvoRivit = () => {
+const ArvoRivit = (rivienMaara, alinLuku, ylinLuku) => {
     // Arvottavat rivit
     let lottoRivi = [];
+    if (ylinLuku - alinLuku < rivienMaara) {
+        console.log("Virhe! Arvottavien rivien määrä on suurempi kuin arvottavien lukujen väli");
+        return;
+    }
 
-    for (let i = 0; i < RIVIT_MAARA; i++) {
-        var luku = Math.floor(Math.random() * RIVIT_YLIN) + 1;
+    for (let i = 0; i < rivienMaara; i++) {
+        var luku = Math.floor(Math.random() * ylinLuku) + 1;
         // Jos arvottu luku löytyy riviltä, arvotaan se uudelleen
-        while (lottoRivi.includes(luku)) {
-            luku = Math.floor(Math.random() * RIVIT_YLIN) + 1;
+        while (lottoRivi.includes(luku) || luku < alinLuku) {
+            luku = Math.floor(Math.random() * ylinLuku) + 1;
         }
         lottoRivi.push(luku);
     }
@@ -25,12 +26,13 @@ const ArvoRivit = () => {
             }
         }
     }
-    LuoTaulukko(lottoRivi);
+    LuoTaulukko(lottoRivi, alinLuku, ylinLuku);
 }
 
 let taulukkoMaara = 0;
+let soluMaara = 0;
 // Luo uuden taulukon johon lisätään kaikki arvottavat numerot sekä voittorivin korostus
-const LuoTaulukko = (voittoRivit) => {
+const LuoTaulukko = (voittoRivit, alinLuku, ylinLuku) => {
 
     let rivitStr = "Voittava rivi on: <b>";
     for (let i = 0; i < voittoRivit.length; i++) {
@@ -42,9 +44,11 @@ const LuoTaulukko = (voittoRivit) => {
     }
     rivitStr += "</b>"
     const voittoRiviElem = document.createElement("p");
+    voittoRiviElem.id = "lottoRiviTeksti" + taulukkoMaara;
     voittoRiviElem.innerHTML = rivitStr;
     // Luodaan uusi taulukko elementti lottoriveille
     const taulukko = document.createElement("table");
+    taulukko.className = "lottoTaulukko";
     taulukko.id = "lottoTaulukko" + taulukkoMaara;
     taulukkoMaara++;
     const taulukkoDiv = document.getElementById("lottoTaulukot")
@@ -61,16 +65,29 @@ const LuoTaulukko = (voittoRivit) => {
     const korostusVari = document.getElementById("lottoVari").value;
 
     // Lisätään kaikki numerot taulukkoon
-    for (let riviNum = 1; riviNum <= RIVIT_YLIN; riviNum++) {
+    let lisatytRivit = 0;
+    for (let riviNum = alinLuku; riviNum <= ylinLuku; riviNum++) {
         const solu = document.createElement("td");
+        solu.id = "lottoSolu" + soluMaara;
         // Lisätään korostusväri voittonumeroille
         if (voittoRivit.includes(riviNum)) {
             solu.style.backgroundColor = korostusVari;
         }
         solu.innerHTML = riviNum;
         riviElem.appendChild(solu);
+        soluMaara++;
+        lisatytRivit++;
         // Uusi rivi joka 6 solua kohtaan
-        if (riviNum % 6 === 0 || riviNum === RIVIT_YLIN) {
+        if (lisatytRivit % 6 === 0 || riviNum === ylinLuku) {
+            if (riviNum === ylinLuku) {
+                // Lisätään tyhjät solut
+                for (let i = 0; i < lisatytRivit%6; i++) {
+                    const solu2 = document.createElement("td");
+                    solu2.id = "lottoSolu" + soluMaara;
+                    riviElem.appendChild(solu2);
+                    soluMaara++;
+                }
+            }
             taulukko.appendChild(riviElem);
             riviElem =  document.createElement("tr");
         }
